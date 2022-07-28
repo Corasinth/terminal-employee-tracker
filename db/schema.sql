2c90f3dd16ed8243b101d0b07e748cceeced073c
Corasinth <beforeyouknowkindness@gmail.com>
2022-07-28 00:09:54 -0500

    fix sql syntax errors

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,23 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR (30)
+);
+ALTER TABLE departments AUTO_INCREMENT = 1;
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30),
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
+);
+ALTER TABLE roles AUTO_INCREMENT = 100; 
+
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR (30), 
@@ -14,21 +31,4 @@ CREATE TABLE employees (
     ON DELETE SET NULL
 );
 ALTER TABLE employees AUTO_INCREMENT = 1000;
-
-CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR (30),
-    salary DECIMAL NOT NULL,
-    department_id INT,
-    FOREIGN KEY (department_id)
-    REFERENCES departments(id)
-    ON DELETE SET NULL
-);
-ALTER TABLE roles AUTO_INCREMENT = 10; 
-
-CREATE TABLE departments (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR (30),
-);
-ALTER TABLE departments AUTO_INCREMENT = 1;
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,36 +1,33 @@
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-Values
-('Sheev', 'Palpatine', 10)
-
+INSERT INTO departments (department_name)
+VALUES
+('Administration'),
+('Finance'),
+('Legal'),
+('Human Resources'),
+('Research & Development'),
+('Maintenance'),
+('Marketing'),
+('Information Technology'),
+('Quality'),
+('Sales');
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
-('Manager')
-('Engineer')
-('Attorney')
-('Intern')
-('Secretary')
-('Janitor')
-('Accountant')
-('HR Representative')
-('Researcher')
-('Advertiser')
-('IT Professional')
-('Sales')
-('Inspector')
-('Software Engineer')
+('Manager', 80000, 1),
+('Engineer', 100000, 5),
+('Attorney', 150000, 3),
+('Intern',30000, NULL),
+('Secretary', 40000, 1),
+('Janitor', 40000, 6),
+('Accountant', 60000, 2),
+('HR Representative', 60000, 4),
+('Researcher', 70000, 5),
+('Advertiser',60000, 7),
+('IT Professional', 50000, 8),
+('Inspector', 90000, 9),
+('Software Engineer', 100000, 5),
+('Salesperson', 60000, 10);
 
-
-
-INSERT INTO departments (department_name)
-VALUES
-('Administration')
-('Finance')
-('Legal')
-('Finance')
-('Human Resources')
-('Research & Development')
-('Maintenance')
-('Marketing')
-('Information Technology')
-('Quality')
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+Values
+('Sheev', 'Palpatine', 100, NULL);
